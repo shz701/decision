6583969c1a151e84ab3db0af67c3c3334ae93522
sunhz <shz@cnfanews.com>
2018-08-06 10:01:54 +0800

    首页修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,6 @@
 //index.js
 //获取应用实例
+var util=require("../../utils/util.js")
 var app = getApp()
 Page({
   data: {
@@ -35,50 +36,26 @@ Page({
   bindAuthLogin:function(e){
     console.log('登录')
     var that = this;
-    wx.login({
-      timeout:30000,
-      success: function (res) {
-        console.log('登录失败！' + res.errMsg)
-        if (res.code) {
-          console.log('获取code成功！' + res.code)
-          var url ='https://spapi.hzfanews.com/api/Login';
-          wx.request({
-            url: url,
-            method:"POST",
-            data:{
-              code:res.code
-            },
-            success:function(res){
-              if(res.data.state===0)
-              {
-                wx.setStorage({
-                  key: "user",
-                  data: res.data.obj
-                })
-                wx.showToast({
-                  title: '登录成功',
-                  icon: 'success',
-                  duration: 2000
-                })
-                console.log('登录成功！' +JSON.stringify(res.data.obj))
-              }
-              else
-              {
-                wx.showToast({
-                  title: '登录失败'+res.data,
-                  icon: 'success',
-                  duration: 2000
-                })
-                console.log('登录失败！' +JSON.stringify(res.data.msg))
-              }
-            }
-          })
-        } else {
-          console.log('登录失败！' + res.errMsg)
-        }
-      },
-      fail(res){
-        console.log('登录失败！' + res.errMsg)
+    util.login(function(res){
+      if (res.data.state === 0) {
+        wx.setStorage({
+          key: "user",
+          data: res.data.obj
+        })
+        wx.showToast({
+          title: '登录成功',
+          icon: 'success',
+          duration: 2000
+        })
+        console.log('登录成功！' + JSON.stringify(res.data.obj))
+      }
+      else {
+        wx.showToast({
+          title: '登录失败' + res.data,
+          icon: 'success',
+          duration: 2000
+        })
+        console.log('登录失败！' + JSON.stringify(res.data.msg))
       }
     })
   },
@@ -87,8 +64,7 @@ Page({
     wx.login({
       
       success:function(res){
-        console.log(res.code) 
-      }
-    })
-  }
+        console.log(res.code)
+	}
+  })
 })
